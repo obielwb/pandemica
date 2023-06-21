3e47729fe4409c1b2f467791e0f19145c141e821
ojpbarbosa <joaopedrofbarbosa@protonmail.com>
2023-06-21 17:44:58 -0300

    feat(update_previosus_neighbor_data): add cpp test

---
+++ test/individuals/update_previous_neighbor_data/cpp/non_optimized/main.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <vector>
+#include <chrono>
+
+using namespace std;
+using namespace chrono;
+
+class Individual
+{
+public:
+  int data;
+  string risk_profile;
+  string interaction;
+  int person_count;
+  string symptoms_checked;
+  string setting;
+  string distance;
+  int duration;
+  string their_mask;
+  string your_mask;
+  string voice;
+
+  void update(int dataclass, string riskprofile, string interaction, int personcount,
+              string symptomschecked, string setting, string distance, int duration,
+              string theirmask, string yourmask, string voice)
+  {
+    data = dataclass;
+    risk_profile = riskprofile;
+    interaction = interaction;
+    person_count = personcount;
+    symptoms_checked = symptomschecked;
+    setting = setting;
+    distance = distance;
+    duration = duration;
+    their_mask = theirmask;
+    your_mask = yourmask;
+    voice = voice;
+  }
+
+  void compute_neighbor(int neighbor_data)
+  {
+    if (neighbor_data == 0)
+    {
+      data = 1;
+    }
+    else
+    {
+      data = 0;
+    }
+  }
+};
+
+int main()
+{
+  vector<Individual> instances(1223237);
+  size_t size = sizeof(instances[0]) * instances.size(); // average: 342506360 bytes - 342 mb
+
+  time_point start = high_resolution_clock::now();
+
+  // process each window of three instances
+  for (size_t i = 1; i < instances.size() - 1; i++)
+  {
+    Individual &prev_instance = instances[i - 1];
+    Individual &current_instance = instances[i];
+    Individual &next_instance = instances[i + 1];
+    current_instance.compute_neighbor(prev_instance.data);
+  }
+
+  time_point end = high_resolution_clock::now();
+  seconds duration = duration_cast<seconds>(end - start);
+
+  cout << duration.count() << " seconds" << endl; // average: 0.012 seconds
+
+  cout << size << endl;
+
+  return 0;
+}
