1b493d6a1426e2a0b73ebca8c0f1aaf50fb6af8e
ojpbarbosa <joaopedrofbarbosa@protonmail.com>
2023-06-21 17:41:08 -0300

    refactor(main.py): rename parameters

--- test/individuals/update_previous_neighbor_data/python/non_optimized/main.py
+++ test/individuals/update_previous_neighbor_data/python/non_optimized/main.py
@@ -6,31 +6,32 @@ import time
 class Individual:
     def __init__(self):
         self.data = 0
-        self.riskProfile = "riskprofile"
+        self.risk_profile = "riskprofile"
         self.interaction = "interaction"
-        self.personCount = 0
-        self.symptomsChecked = "no"
+        self.person_count = 0
+        self.symptoms_checked = "no"
 
         self.setting = "setting"
         self.distance = "distance"
         self.duration = 0
-        self.theirMask = "theirmask"
-        self.yourMask = "yourmask"
+        self.their_mask = "their_mask"
+        self.your_mask = "your_mask"
         self.voice = "voice"
 
-    def update(self, a, b, c, d, e, f, g, h, i, j):
-        self.data += a
-        self.riskProfile = b
-        self.interaction = c
-        self.personCount = d
-        self.symptomsChecked = e
+    def update(self, data, risk_profile, interaction, person_count,
+               symptoms_checked, setting, distance, duration, their_mask, your_mask, voice):
+        self.data = data
+        self.risk_profile = risk_profile
+        self.interaction = interaction
+        self.person_count = person_count
+        self.symptoms_checked = symptoms_checked
 
-        self.setting = f
-        self.distance = g
-        self.duration = 0
-        self.theirMask = h
-        self.yourMask = i
-        self.voice = j
+        self.setting = setting
+        self.distance = distance
+        self.duration = duration
+        self.their_mask = their_mask
+        self.your_mask = your_mask
+        self.voice = voice
 
     def compute_neighbor(self, neighbor_data):
         if neighbor_data == 0:
@@ -42,17 +43,17 @@ class Individual:
 instances = [Individual() for _ in range(1223237)]
 size = sys.getsizeof(instances)  # average: 10692984 bytes - 10 mb
 
-# Create a sliding window of size 3 over the instances
+# create a sliding window of size 3 over the instances
 windows = itertools.islice(
     zip([None] + instances, instances, instances[1:] + [None]), 1, None)
 
 start = time.time()
-# Process each window of three instances
+# process each window of three instances
 for prev_instance, current_instance, next_instance in windows:
     current_instance.compute_neighbor(
         prev_instance.data if prev_instance else 0)
 
 end = time.time()
-print(end - start)  # average: 0.22687172889709473 seconds
+print(end - start)  # average: 0.22 seconds
 
 print(size)
