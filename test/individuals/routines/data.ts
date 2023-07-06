194cf595c114fe36cd123d42a43122da956600b4
Gabriel W. Bartmanovicz <obielwb@gmail.com>
2023-07-06 12:30:59 -0300

    feat(individuals/routines): create data.ts

---
+++ test/individuals/routines/data.ts
@@ -0,0 +1,27 @@
+/*
+ * generate random age
+ * generate random sex
+ * generate activities:
+ *    - search in microCovid to see if what they have
+ * generate basic routines based on age, sex, location, etc
+ *    variables:
+ *    - house location
+ *    - have car / dont have car
+ *    - work location
+ *    - school location
+ *    -
+ *
+ *
+ * for simplicity, the population does not have a growth rate
+ */
+
+// Relatório de Informações Sociais 2016
+const totalPopulation = 1138309;
+const eastPopulation = 248939;
+const northwestPopulation = 133086;
+const northPopulation = 212342;
+const southwestPopulation = 253061;
+const southPopulation = 316671;
+
+const malePercentage = 51.78;
+const femalePercentage = 48.22;
