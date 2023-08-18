27fec140976e0a5b6ab4657e40d21a3cdbef889e
ojpbarbosa <joaopedrofbarbosa@protonmail.com>
2023-08-18 11:26:46 -0300

    style(*.ts): use prettier

--- test/individuals/routines/data.ts
+++ test/individuals/routines/data.ts
@@ -16,138 +16,138 @@
  */
 
 // Relatório de Informações Sociais 2016
-export const totalPopulation = 1138309;
-export const eastPopulation = 248939;
-export const northwestPopulation = 133086;
-export const northPopulation = 212342;
-export const southwestPopulation = 253061;
-export const southPopulation = 316671;
+export const totalPopulation = 1138309
+export const eastPopulation = 248939
+export const northwestPopulation = 133086
+export const northPopulation = 212342
+export const southwestPopulation = 253061
+export const southPopulation = 316671
 
-export const malePercentage = 51.78;
-export const femalePercentage = 48.22;
+export const malePercentage = 51.78
+export const femalePercentage = 48.22
 
 // IBGE 2010
 export const zeroToFourHabitants = {
   female: 31330,
-  male: 32541,
-};
+  male: 32541
+}
 
 export const fiveToNineHabitants = {
   female: 32214,
-  male: 33570,
-};
+  male: 33570
+}
 
 export const tenToFourteenHabitants = {
   female: 38690,
-  male: 39891,
-};
+  male: 39891
+}
 
 export const fifteenToNineteenHabitants = {
   female: 40346,
-  male: 41344,
-};
+  male: 41344
+}
 
 export const twentyToTwentyfourHabitants = {
   female: 48593,
-  male: 48225,
-};
+  male: 48225
+}
 
 export const twentyfiveToTwentynineHabitants = {
   female: 52883,
-  male: 51178,
-};
+  male: 51178
+}
 
 export const thirtyToThirtyfourHabitants = {
   female: 49301,
-  male: 47315,
-};
+  male: 47315
+}
 
 export const thirtyfiveToThirtynineHabitants = {
   female: 43304,
-  male: 40812,
-};
+  male: 40812
+}
 
 export const fourtyToFourtyfourHabitants = {
   female: 41335,
-  male: 37975,
-};
+  male: 37975
+}
 
 export const fourtyfiveToFourtynineHabitants = {
   female: 38876,
-  male: 34630,
-};
+  male: 34630
+}
 
 export const fiftyToFiftyfourHabitants = {
   female: 35795,
-  male: 31354,
-};
+  male: 31354
+}
 
 export const fiftyfiveToFiftynineHabitants = {
   female: 29515,
-  male: 25295,
-};
+  male: 25295
+}
 
 export const sixtyToSixtyfourHabitants = {
   female: 23211,
-  male: 19059,
-};
+  male: 19059
+}
 
 export const sixtyfiveToSixtynineHabitants = {
   female: 16986,
-  male: 13776,
-};
+  male: 13776
+}
 
 export const seventyToSeventyfourHabitants = {
   female: 13743,
-  male: 10457,
-};
+  male: 10457
+}
 
 export const seventyfiveToSeventynineHabitants = {
   female: 6832,
-  male: 10260,
-};
+  male: 10260
+}
 
 export const eightyToEightyfourHabitants = {
   female: 7291,
-  male: 4094,
-};
+  male: 4094
+}
 
 // IBGE 2022
-export const car = 623743;
-export const motorcycle = 130933;
-export const bus = 5418;
-export const uttility = 13218;
+export const car = 623743
+export const motorcycle = 130933
+export const bus = 5418
+export const uttility = 13218
 
 // IBGE 2010 - Habitantes por faixa de renda
-export const zeroToHalfBasicSalary = 10400;
-export const halfToOneBasicSalary = 98800;
-export const oneToTwoBasicSalary = 225500;
-export const twoToThreeBasicSalary = 98600;
-export const threeToFiveBasicSalary = 89300;
-export const fiveToTenBasicSalary = 72300;
-export const tenToFifteenBasicSalary = 15700;
-export const fifteenToTwentyBasicSalary = 13400;
-export const twentyOrMoreBasicSalary = 10400;
-export const withoutSalary = 316000;
+export const zeroToHalfBasicSalary = 10400
+export const halfToOneBasicSalary = 98800
+export const oneToTwoBasicSalary = 225500
+export const twoToThreeBasicSalary = 98600
+export const threeToFiveBasicSalary = 89300
+export const fiveToTenBasicSalary = 72300
+export const tenToFifteenBasicSalary = 15700
+export const fifteenToTwentyBasicSalary = 13400
+export const twentyOrMoreBasicSalary = 10400
+export const withoutSalary = 316000
 
 // IBGE 2010 - Frequência em escola ou crechê
-export const attendSchools = 324646;
-export const ageZeroToThree = 21685;
-export const ageFourToFive = 21201;
-export const ageSix = 12535;
-export const ageSevenToFourteen = 113766;
-export const ageFifteenToSeventeen = 41497;
-export const ageEighteenToNineteen = 15200;
-export const ageTwentyToTwentyFour = 29289;
-export const ageTwentyfiveOrMore = 69474;
-export const alreadyAttended = 687104;
-export const neverAttended = 68363;
+export const attendSchools = 324646
+export const ageZeroToThree = 21685
+export const ageFourToFive = 21201
+export const ageSix = 12535
+export const ageSevenToFourteen = 113766
+export const ageFifteenToSeventeen = 41497
+export const ageEighteenToNineteen = 15200
+export const ageTwentyToTwentyFour = 29289
+export const ageTwentyfiveOrMore = 69474
+export const alreadyAttended = 687104
+export const neverAttended = 68363
 export interface Activity {
   // Activity Risk
-  setting: string;
-  distance: string;
-  duration?: number; // minutes
-  voice: string;
+  setting: string
+  distance: string
+  duration?: number // minutes
+  voice: string
 
   // Activity Setting
   // coordinate: {
@@ -158,77 +158,77 @@ export interface Activity {
 
 export const activities: { [key: string]: Activity } = {
   shopping: {
-    setting: "indoor",
-    distance: "sixFt",
+    setting: 'indoor',
+    distance: 'sixFt',
     duration: 60,
-    voice: "silent",
+    voice: 'silent'
   },
   restaurantOutdoors: {
-    setting: "outdoor",
-    distance: "sixFt",
+    setting: 'outdoor',
+    distance: 'sixFt',
     duration: 90,
-    voice: "normal",
+    voice: 'normal'
   },
   restaurantIndoors: {
-    setting: "outdoor",
-    distance: "sixFt",
+    setting: 'outdoor',
+    distance: 'sixFt',
     duration: 90,
-    voice: "normal",
+    voice: 'normal'
   },
   bar: {
-    setting: "indoor",
-    distance: "sixFt",
+    setting: 'indoor',
+    distance: 'sixFt',
     duration: 120,
-    voice: "loud",
+    voice: 'loud'
   },
   outdoorParty: {
-    setting: "outdoor",
-    distance: "normal",
+    setting: 'outdoor',
+    distance: 'normal',
     duration: 180,
-    voice: "loud",
+    voice: 'loud'
   },
   indoorParty: {
-    setting: "indoor",
-    distance: "normal",
+    setting: 'indoor',
+    distance: 'normal',
     duration: 180,
-    voice: "normal",
+    voice: 'normal'
   },
   house: {
-    setting: "indoor",
-    distance: "intimate",
+    setting: 'indoor',
+    distance: 'intimate',
     duration: 600,
-    voice: "normal",
+    voice: 'normal'
   },
   school: {
-    setting: "indoor",
-    distance: "normal",
+    setting: 'indoor',
+    distance: 'normal',
     duration: 480, // 8 hours
-    voice: "normal",
+    voice: 'normal'
   },
   hospital: {
-    setting: "indoor",
-    distance: "sixFt",
-    voice: "normal",
+    setting: 'indoor',
+    distance: 'sixFt',
+    voice: 'normal'
   },
   publicTransportStation: {
-    setting: "outdoor",
-    distance: "normal",
-    voice: "normal",
-    duration: 60,
+    setting: 'outdoor',
+    distance: 'normal',
+    voice: 'normal',
+    duration: 60
   },
   publicTransportStationCrowded: {
-    setting: "outdoor",
-    distance: "close",
-    voice: "normal",
-    duration: 60,
+    setting: 'outdoor',
+    distance: 'close',
+    voice: 'normal',
+    duration: 60
   },
   parks: {
-    setting: "outdoor",
-    distance: "tenFt",
-    voice: "silent",
-    duration: 90,
-  },
-};
+    setting: 'outdoor',
+    distance: 'tenFt',
+    voice: 'silent',
+    duration: 90
+  }
+}
 
 /*
 Questões em aberto:
--- test/microcovid/prepopulated.ts
+++ test/microcovid/prepopulated.ts
@@ -1,331 +1,331 @@
-import { CalculatorData } from "./calculate";
+import { CalculatorData } from './calculate'
 
 export type PartialData = Omit<
   CalculatorData,
-  | "riskBudget"
-  | "useManualEntry"
-  | "topLocation"
-  | "subLocation"
-  | "subSubLocation"
-  | "population"
-  | "casesPastWeek"
-  | "casesIncreasingPercentage"
-  | "positiveCasePercentage"
-  | "prevalanceDataDate"
-  | "percentFullyVaccinated"
-  | "unvaccinatedPrevalenceRatio"
-  | "averageFullyVaccinatedMultiplier"
-  | "yourVaccineType"
-  | "yourVaccineDoses"
->;
+  | 'riskBudget'
+  | 'useManualEntry'
+  | 'topLocation'
+  | 'subLocation'
+  | 'subSubLocation'
+  | 'population'
+  | 'casesPastWeek'
+  | 'casesIncreasingPercentage'
+  | 'positiveCasePercentage'
+  | 'prevalanceDataDate'
+  | 'percentFullyVaccinated'
+  | 'unvaccinatedPrevalenceRatio'
+  | 'averageFullyVaccinatedMultiplier'
+  | 'yourVaccineType'
+  | 'yourVaccineDoses'
+>
 
 // duração é em minutos
 
 export const prepopulated: {
-  [key: string]: PartialData;
+  [key: string]: PartialData
 } = {
   outdoorMasked2: {
-    riskProfile: "average", // individuo
-    interaction: "oneTime",
+    riskProfile: 'average', // individuo
+    interaction: 'oneTime',
     personCount: 2,
-    symptomsChecked: "no", // individuo / vizinho
+    symptomsChecked: 'no', // individuo / vizinho
 
-    setting: "outdoor", // ambiente
-    distance: "normal", // ambiente
+    setting: 'outdoor', // ambiente
+    distance: 'normal', // ambiente
     duration: 60, // ambiente
-    theirMask: "basic", // informação do vizinho
-    yourMask: "basic", // informação do indivíduo
-    voice: "normal", // ambiente
-    theirVaccine: "undefined", // vizinho
-    scenarioName: "outdoorMasked2",
+    theirMask: 'basic', // informação do vizinho
+    yourMask: 'basic', // informação do indivíduo
+    voice: 'normal', // ambiente
+    theirVaccine: 'undefined', // vizinho
+    scenarioName: 'outdoorMasked2'
   },
 
   indoorUnmasked2: {
-    riskProfile: "average",
-    interaction: "oneTime",
+    riskProfile: 'average',
+    interaction: 'oneTime',
     personCount: 2,
-    symptomsChecked: "no",
+    symptomsChecked: 'no',
 
-    setting: "indoor",
-    distance: "normal",
+    setting: 'indoor',
+    distance: 'normal',
     duration: 60,
-    theirMask: "none",
-    yourMask: "none",
-    voice: "normal",
-    theirVaccine: "undefined",
-    scenarioName: "indoorUnmasked2",
+    theirMask: 'none',
+    yourMask: 'none',
+    voice: 'normal',
+    theirVaccine: 'undefined',
+    scenarioName: 'indoorUnmasked2'
   },
 
-  "1person_15minCarRide": {
-    riskProfile: "average",
-    interaction: "oneTime",
+  '1person_15minCarRide': {
+    riskProfile: 'average',
+    interaction: 'oneTime',
     personCount: 1,
-    symptomsChecked: "no",
+    symptomsChecked: 'no',
 
-    setting: "indoor",
-    distance: "normal",
+    setting: 'indoor',
+    distance: 'normal',
     duration: 15,
-    theirMask: "none",
-    yourMask: "none",
-    voice: "normal",
-    theirVaccine: "undefined",
-    scenarioName: "1person_15minCarRide",
+    theirMask: 'none',
+    yourMask: 'none',
+    voice: 'normal',
+    theirVaccine: 'undefined',
+    scenarioName: '1person_15minCarRide'
   },
 
   oneNightStand: {
-    riskProfile: "average",
-    interaction: "oneTime",
+    riskProfile: 'average',
+    interaction: 'oneTime',
     personCount: 1,
-    symptomsChecked: "no",
+    symptomsChecked: 'no',
 
-    setting: "indoor",
-    distance: "intimate",
+    setting: 'indoor',
+    distance: 'intimate',
     duration: 600,
-    theirMask: "none",
-    yourMask: "none",
-    voice: "normal",
-    theirVaccine: "undefined",
-    scenarioName: "oneNightStand",
+    theirMask: 'none',
+    yourMask: 'none',
+    voice: 'normal',
+    theirVaccine: 'undefined',
+    scenarioName: 'oneNightStand'
   },
 
   liveInPartner_noContacts: {
-    riskProfile: "livingAlone",
-    interaction: "partner",
+    riskProfile: 'livingAlone',
+    interaction: 'partner',
     personCount: 1,
-    symptomsChecked: "no",
+    symptomsChecked: 'no',
 
-    setting: "indoor",
-    distance: "intimate",
+    setting: 'indoor',
+    distance: 'intimate',
     duration: 600,
-    theirMask: "none",
-    yourMask: "none",
-    voice: "normal",
-    theirVaccine: "undefined",
-    scenarioName: "liveInPartner_noContacts",
+    theirMask: 'none',
+    yourMask: 'none',
+    voice: 'normal',
+    theirVaccine: 'undefined',
+    scenarioName: 'liveInPartner_noContacts'
   },
 
-  "60minShopping": {
-    riskProfile: "average",
-    interaction: "oneTime",
+  '60minShopping': {
+    riskProfile: 'average',
+    interaction: 'oneTime',
     personCount: 5,
-    symptomsChecked: "no",
+    symptomsChecked: 'no',
 
-    setting: "indoor",
-    distance: "sixFt",
+    setting: 'indoor',
+    distance: 'sixFt',
     duration: 60,
-    theirMask: "basic",
-    yourMask: "basic",
-    voice: "silent",
-    theirVaccine: "undefined",
-    scenarioName: "60minShopping",
+    theirMask: 'basic',
+    yourMask: 'basic',
+    voice: 'silent',
+    theirVaccine: 'undefined',
+    scenarioName: '60minShopping'
   },
 
-  "60minShoppingFew": {
-    riskProfile: "average",
-    interaction: "oneTime",
+  '60minShoppingFew': {
+    riskProfile: 'average',
+    interaction: 'oneTime',
     personCount: 3,
-    symptomsChecked: "no",
+    symptomsChecked: 'no',
 
-    setting: "indoor",
-    distance: "sixFt",
+    setting: 'indoor',
+    distance: 'sixFt',
     duration: 60,
-    theirMask: "basic",
-    yourMask: "basic",
-    voice: "silent",
-    theirVaccine: "undefined",
-    scenarioName: "60minShoppingFew",
+    theirMask: 'basic',
+    yourMask: 'basic',
+    voice: 'silent',
+    theirVaccine: 'undefined',
+    scenarioName: '60minShoppingFew'
   },
 
-  "60minShoppingCrowded": {
-    riskProfile: "average",
-    interaction: "oneTime",
+  '60minShoppingCrowded': {
+    riskProfile: 'average',
+    interaction: 'oneTime',
     personCount: 10,
-    symptomsChecked: "no",
+    symptomsChecked: 'no',
 
-    setting: "indoor",
-    distance: "sixFt",
+    setting: 'indoor',
+    distance: 'sixFt',
     duration: 60,
-    theirMask: "basic",
-    yourMask: "basic",
-    voice: "silent",
-    theirVaccine: "undefined",
-    scenarioName: "60minShoppingCrowded",
+    theirMask: 'basic',
+    yourMask: 'basic',
+    voice: 'silent',
+    theirVaccine: 'undefined',
+    scenarioName: '60minShoppingCrowded'
   },
 
   planeRide: {
-    riskProfile: "average",
-    interaction: "oneTime",
+    riskProfile: 'average',
+    interaction: 'oneTime',
     personCount: 41,
-    symptomsChecked: "no",
+    symptomsChecked: 'no',
 
-    setting: "plane",
-    distance: "sixFt",
+    setting: 'plane',
+    distance: 'sixFt',
     duration: 360,
-    theirMask: "basic",
-    yourMask: "basic",
-    voice: "silent",
-    theirVaccine: "undefined",
-    scenarioName: "planeRide",
+    theirMask: 'basic',
+    yourMask: 'basic',
+    voice: 'silent',
+    theirVaccine: 'undefined',
+    scenarioName: 'planeRide'
   },
 
   planeRideMiddleSeatEmpty: {
-    riskProfile: "average",
-    interaction: "oneTime",
+    riskProfile: 'average',
+    interaction: 'oneTime',
     personCount: 20,
-    symptomsChecked: "no",
+    symptomsChecked: 'no',
 
-    setting: "plane",
-    distance: "sixFt",
+    setting: 'plane',
+    distance: 'sixFt',
     duration: 360,
-    theirMask: "basic",
-    yourMask: "basic",
-    voice: "silent",
-    theirVaccine: "undefined",
-    scenarioName: "planeRideMiddleSeatEmpty",
+    theirMask: 'basic',
+    yourMask: 'basic',
+    voice: 'silent',
+    theirVaccine: 'undefined',
+    scenarioName: 'planeRideMiddleSeatEmpty'
   },
 
   restaurantOutdoors: {
-    riskProfile: "average",
-    interaction: "oneTime",
+    riskProfile: 'average',
+    interaction: 'oneTime',
     personCount: 15,
-    symptomsChecked: "no",
+    symptomsChecked: 'no',
 
-    setting: "outdoor",
-    distance: "sixFt",
+    setting: 'outdoor',
+    distance: 'sixFt',
     duration: 90,
-    theirMask: "none",
-    yourMask: "none",
-    voice: "normal",
-    theirVaccine: "undefined",
-    scenarioName: "restaurantOutdoors",
+    theirMask: 'none',
+    yourMask: 'none',
+    voice: 'normal',
+    theirVaccine: 'undefined',
+    scenarioName: 'restaurantOutdoors'
   },
 
   restaurantIndoors: {
-    riskProfile: "average",
-    interaction: "oneTime",
+    riskProfile: 'average',
+    interaction: 'oneTime',
     personCount: 15,
-    symptomsChecked: "no",
+    symptomsChecked: 'no',
 
-    setting: "indoor",
-    distance: "sixFt",
+    setting: 'indoor',
+    distance: 'sixFt',
     duration: 90,
-    theirMask: "none",
-    yourMask: "none",
-    voice: "normal",
-    theirVaccine: "undefined",
-    scenarioName: "restaurantIndoors",
+    theirMask: 'none',
+    yourMask: 'none',
+    voice: 'normal',
+    theirVaccine: 'undefined',
+    scenarioName: 'restaurantIndoors'
   },
 
   bar: {
-    riskProfile: "average",
-    interaction: "oneTime",
+    riskProfile: 'average',
+    interaction: 'oneTime',
     personCount: 15,
-    symptomsChecked: "no",
+    symptomsChecked: 'no',
 
-    setting: "indoor",
-    distance: "sixFt",
+    setting: 'indoor',
+    distance: 'sixFt',
     duration: 120,
-    theirMask: "none",
-    yourMask: "none",
-    voice: "loud",
-    theirVaccine: "undefined",
-    scenarioName: "bar",
+    theirMask: 'none',
+    yourMask: 'none',
+    voice: 'loud',
+    theirVaccine: 'undefined',
+    scenarioName: 'bar'
   },
 
   largeOutdoorParty: {
-    riskProfile: "average",
-    interaction: "oneTime",
+    riskProfile: 'average',
+    interaction: 'oneTime',
     personCount: 80,
-    symptomsChecked: "no",
+    symptomsChecked: 'no',
 
-    setting: "outdoor",
-    distance: "normal",
+    setting: 'outdoor',
+    distance: 'normal',
     duration: 180,
-    theirMask: "basic",
-    yourMask: "basic",
-    voice: "normal",
-    theirVaccine: "undefined",
-    scenarioName: "largeOutdoorParty",
+    theirMask: 'basic',
+    yourMask: 'basic',
+    voice: 'normal',
+    theirVaccine: 'undefined',
+    scenarioName: 'largeOutdoorParty'
   },
 
   smallIndoorParty25: {
-    riskProfile: "average",
-    interaction: "oneTime",
+    riskProfile: 'average',
+    interaction: 'oneTime',
     personCount: 25,
-    symptomsChecked: "no",
+    symptomsChecked: 'no',
 
-    setting: "indoor",
-    distance: "normal",
+    setting: 'indoor',
+    distance: 'normal',
     duration: 180,
-    theirMask: "none",
-    yourMask: "none",
-    voice: "normal",
-    theirVaccine: "undefined",
-    scenarioName: "smallIndoorParty25",
+    theirMask: 'none',
+    yourMask: 'none',
+    voice: 'normal',
+    theirVaccine: 'undefined',
+    scenarioName: 'smallIndoorParty25'
   },
 
   outdoorMaskedWithCovidPositive: {
-    riskProfile: "hasCovid",
-    interaction: "oneTime",
+    riskProfile: 'hasCovid',
+    interaction: 'oneTime',
     personCount: 1,
-    symptomsChecked: "no",
+    symptomsChecked: 'no',
 
-    setting: "outdoor",
-    distance: "normal",
+    setting: 'outdoor',
+    distance: 'normal',
     duration: 60,
-    theirMask: "basic",
-    yourMask: "basic",
-    voice: "normal",
-    theirVaccine: "undefined",
-    scenarioName: "outdoorMaskedWithCovidPositive",
+    theirMask: 'basic',
+    yourMask: 'basic',
+    voice: 'normal',
+    theirVaccine: 'undefined',
+    scenarioName: 'outdoorMaskedWithCovidPositive'
   },
 
   indoorUnmaskedWithCovidPositive: {
-    riskProfile: "hasCovid",
-    interaction: "oneTime",
+    riskProfile: 'hasCovid',
+    interaction: 'oneTime',
     personCount: 1,
-    symptomsChecked: "no",
+    symptomsChecked: 'no',
 
-    setting: "indoor",
-    distance: "normal",
+    setting: 'indoor',
+    distance: 'normal',
     duration: 60,
-    theirMask: "none",
-    yourMask: "none",
-    voice: "normal",
-    theirVaccine: "undefined",
-    scenarioName: "indoorUnmaskedWithCovidPositive",
+    theirMask: 'none',
+    yourMask: 'none',
+    voice: 'normal',
+    theirVaccine: 'undefined',
+    scenarioName: 'indoorUnmaskedWithCovidPositive'
   },
 
   votingInPerson: {
-    riskProfile: "average",
-    interaction: "oneTime",
+    riskProfile: 'average',
+    interaction: 'oneTime',
     personCount: 2,
-    symptomsChecked: "no",
+    symptomsChecked: 'no',
 
-    setting: "indoor",
-    distance: "sixFt",
+    setting: 'indoor',
+    distance: 'sixFt',
     duration: 10,
-    theirMask: "basic",
-    yourMask: "basic",
-    voice: "silent",
-    theirVaccine: "undefined",
-    scenarioName: "votingInPerson",
+    theirMask: 'basic',
+    yourMask: 'basic',
+    voice: 'silent',
+    theirVaccine: 'undefined',
+    scenarioName: 'votingInPerson'
   },
   custom: {
     // This special profile is applied to reset the rest of the calculator.
     // Keep at the end of this list.
-    riskProfile: "",
-    interaction: "",
+    riskProfile: '',
+    interaction: '',
     personCount: 0,
-    symptomsChecked: "no",
+    symptomsChecked: 'no',
 
-    setting: "",
-    distance: "",
+    setting: '',
+    distance: '',
     duration: 0,
-    theirMask: "",
-    yourMask: "",
-    voice: "",
-    theirVaccine: "undefined",
-    scenarioName: "custom",
-  },
-};
+    theirMask: '',
+    yourMask: '',
+    voice: '',
+    theirVaccine: 'undefined',
+    scenarioName: 'custom'
+  }
+}
