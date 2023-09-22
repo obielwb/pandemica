073c0f87ed9fa6e25413b9e67f67ea2487361e07
ojpbarbosa <joaopedrofbarbosa@protonmail.com>
2023-09-22 11:20:09 -0300

    chore(routines): start residents per house test

--- individuals/routines/test/parameter.test.ts
+++ individuals/routines/test/parameter.test.ts
@@ -1,7 +1,7 @@
 import { describe, expect, test } from 'bun:test'
 
 import { normalize } from '../parameter'
-import { totalPopulation, regionsPopulation } from '../data'
+import { totalPopulation, regionsPopulation, residentsPerHouse } from '../data'
 import { Individual } from '../individual'
 
 const individuals = Array.from({ length: totalPopulation }, () => new Individual())
@@ -10,7 +10,7 @@ describe('Parameter', () => {
   test('Should differ if regions are not normalized', () => {
     const regionsIndividuals = regionsPopulation.reduce((acc, region) => acc + region.value, 0)
 
-    expect(totalPopulation).not.toBe(regionsIndividuals)
+    expect(regionsIndividuals).not.toBe(totalPopulation)
   })
 
   test('Should normalize regions population', () => {
@@ -20,6 +20,43 @@ describe('Parameter', () => {
       0
     )
 
-    expect(totalPopulation).toBe(normalizedRegionsIndividuals)
+    expect(normalizedRegionsIndividuals).toBe(totalPopulation)
+  })
+
+  test('Should normalize residents per house', () => {
+    const labeledIndividuals = residentsPerHouse.reduce((acc, p) => acc + p.value * p.label, 0)
+
+    let labelPercentages = residentsPerHouse.map((p) => (p.value * p.label) / labeledIndividuals)
+
+    const unlabeledIndividuals = individuals.length - labeledIndividuals
+
+    const normalizedLabels = residentsPerHouse.map((p, i) => {
+      const labelPercentage = labelPercentages[i]
+      return {
+        label: p.label,
+        value: Math.round(p.value + unlabeledIndividuals * labelPercentage)
+      }
+    })
+
+    console.log(
+      normalizedLabels.reduce((acc, p) => acc + (p.value * p.label) / individuals.length, 0)
+    )
+
+    // const normalizedLabeledIndividuals = normalizedLabels.reduce(
+    //   (acc, normalizedLabel) => acc + normalizedLabel.value,
+    //   0
+    // )
+
+    // let stillUnlabeledIndividuals = individuals.length - normalizedLabeledIndividuals
+    // while (stillUnlabeledIndividuals > 0) {
+    //   for (
+    //     let i = 0;
+    //     i < stillUnlabeledIndividuals / parameter.length;
+    //     i = (i + 1) % normalizedLabels.length
+    //   ) {
+    //     normalizedLabels[i].value++
+    //     stillUnlabeledIndividuals--
+    //   }
+    // }
   })
 })
