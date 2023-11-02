59dd2e1b4aeb4e838609ccc85743fcd35a4d9de9
Gabriel W. Bartmanovicz <obielwb@gmail.com>
2023-11-02 11:29:45 -0300

    feat: create prevalence.ts

---
+++ calculator/src/prevalence.ts
@@ -0,0 +1,10 @@
+export type Prevalence = {
+  population: number
+  casesPastWeek: number
+  casesIncreasingPercentage: number
+  positiveCasePercentage: number
+  prevalanceDataDate: Date
+  percentFullyVaccinated: number
+  unvaccinatedPrevalenceRatio: number
+  averageFullyVaccinatedMultiplier: number
+}
