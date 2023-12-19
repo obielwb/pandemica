79818ffec5cdc2e9a1b232cea054301c8fc4adeb
Gabriel W. Bartmanovicz <obielwb@gmail.com>
2023-12-19 17:54:16 -0300

    chore: add comments in calculate.ts

--- src/calculate.ts
+++ src/calculate.ts
@@ -1,3 +1,12 @@
 import { Activity } from './activities'
 
+/*
+
+eu = red_vacina * red_mascara * age
+
+ambiente = setting  * duration * distance
+
+com covid = (red_mascara * voz * red_vacina * 1   )
+
+*/
 export function calculate(activity: Activity) {}
