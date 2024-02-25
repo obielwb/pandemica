52666840e23e0f0fa36f3d1c5faadd8ce81a0a70
Gabriel W. Bartmanovicz <obielwb@gmail.com>
2024-02-25 15:39:08 -0300

    feat(lockdown trigger): implement work from home function

--- src/triggers/lockdown.ts
+++ src/triggers/lockdown.ts
@@ -1,12 +1,22 @@
 import {
   collegeStudyFromHome,
   highSchoolStudyFromHome,
+  largeCommerceAndServicesWorkFromHome,
+  largeIndustryWorkFromHome,
+  mediumCommerceAndServicesWorkFromHome,
+  mediumIndustryWorkFromHome,
+  microCommerceAndServicesWorkFromHome,
+  microIndustryWorkFromHome,
   middleSchoolStudyFromHome,
-  preschoolStudyFromHome
+  preschoolStudyFromHome,
+  smallCommerceAndServicesWorkFromHome,
+  smallIndustryWorkFromHome
 } from '../activities'
 import { Individual } from '../individual'
+import { shuffle } from '../utilities'
 
 const LOCKDOWN_INIT_DATE = '01-03-2020'
+const INDIVIDUALS_WITHOUT_OCCUPATION_RECUPERATION_DATE = ''
 
 const LOCKDOWN_SCHOOL_INIT_PERCENTAGE = 1.0 // x% of all individuals who will be selected to study at home
 const LOCKDOWN_SCHOOL_RECUPERATION_RATE = 0.2 // x% of 100% studying from home individuals
@@ -35,14 +45,43 @@ export function assignLockdown(day: string, month: number, year: number, populat
 
   if (schoolRecuperationsDates.includes(date)) schoolRecuperation(population)
   if (workRecuperationsDates.includes(date)) workRecuperation(population)
+
+  if (INDIVIDUALS_WITHOUT_OCCUPATION_RECUPERATION_DATE === date)
+    individualsWithoutOcuppationRecuperation(population)
 }
 
 function start(population: Individual[]) {
-  for (const individual of population) {
-    if (individual.occupationTypes.includes('study')) implementSchoolFromHome(individual)
+  const shuffledPopulation = shuffle(population)
 
-    if (individual.occupationTypes.includes('work')) implementWorkFromHome(individual)
+  const workers = shuffledPopulation.filter((individual) =>
+    individual.occupationTypes.includes('work')
+  )
+  const students = shuffledPopulation.filter((individual) =>
+    individual.occupationTypes.includes('study')
+  )
+  const withouthOccupationIndividuals = shuffledPopulation.filter(
+    (individual) =>
+      individual.occupationTypes.includes('study') === false &&
+      individual.occupationTypes.includes('work') === false
+  )
+
+  const lockdownWorkers = workers.slice(
+    0,
+    Math.ceil(workers.length * LOCKDOWN_WORK_INIT_PERCENTAGE)
+  )
+  for (const individual of lockdownWorkers) {
+    implementWorkFromHome(individual)
   }
+
+  const lockdownStudents = students.slice(
+    0,
+    Math.ceil(students.length * LOCKDOWN_SCHOOL_INIT_PERCENTAGE)
+  )
+  for (const individual of lockdownStudents) {
+    implementSchoolFromHome(individual)
+  }
+
+  //todo: decrease outside activities, like shopping and leisure
 }
 
 function implementSchoolFromHome(individual: Individual) {
@@ -68,8 +107,43 @@ function implementSchoolFromHome(individual: Individual) {
   }
 }
 
-function implementWorkFromHome(individual: Individual) {}
+function implementWorkFromHome(individual: Individual) {
+  for (let i = 0; i < individual.routine.length; i++) {
+    for (let j = 0; j < individual.routine[i].length; j++) {
+      if (individual.routine[i][j].category === 'work') {
+        switch (individual.routine[i][j].label) {
+          case 'work.industry.micro':
+            individual.routine[i][j] = microIndustryWorkFromHome
+            break
+          case 'work.industry.small':
+            individual.routine[i][j] = smallIndustryWorkFromHome
+            break
+          case 'work.industry.medium':
+            individual.routine[i][j] = mediumIndustryWorkFromHome
+            break
+          case 'work.industry.large':
+            individual.routine[i][j] = largeIndustryWorkFromHome
+            break
+          case 'work.commerce_services.micro':
+            individual.routine[i][j] = microCommerceAndServicesWorkFromHome
+            break
+          case 'work.commerce_services.small':
+            individual.routine[i][j] = smallCommerceAndServicesWorkFromHome
+            break
+          case 'work.commerce_services.medium':
+            individual.routine[i][j] = mediumCommerceAndServicesWorkFromHome
+            break
+          case 'work.commerce_services.large':
+            individual.routine[i][j] = largeCommerceAndServicesWorkFromHome
+            break
+        }
+      }
+    }
+  }
+}
 
 function schoolRecuperation(population: Individual[]) {}
 
 function workRecuperation(population: Individual[]) {}
+
+function individualsWithoutOcuppationRecuperation(population: Individual[]) {}
