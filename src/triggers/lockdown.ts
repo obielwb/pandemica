c94873e54ba5d86d56189fb20baad17c5e3eb4ea
Gabriel W. Bartmanovicz <obielwb@gmail.com>
2024-02-25 14:11:19 -0300

    chore: improve lockdown.ts

--- src/triggers/lockdown.ts
+++ src/triggers/lockdown.ts
@@ -6,52 +6,74 @@ import {
 } from '../activities'
 import { Individual } from '../individual'
 
-const LOCKDOWN_INIT_DATE = ''
+const LOCKDOWN_INIT_DATE = '01-03-2020'
 
-const LOCKDOWN_SCHOOL_INIT_PERCENTAGE = 100
-const LOCKDOWN_START_SCHOOLS_RECUPERATION_DATE = ''
-const LOCKDOWN_SCHOOL_RECUPERATION_RATE_PER_MONTH = 20
+const LOCKDOWN_SCHOOL_INIT_PERCENTAGE = 100 // x% of all individuals who are going to study from home
+const LOCKDOWN_SCHOOL_RECUPERATION_RATE = 20 // x% of 100% studying from home individuals
+const schoolRecuperationsDates = [
+  '01-03-2021',
+  '01-04-2021',
+  '01-05-2021',
+  '01-06-2021',
+  '01-07-2021'
+]
 
-const LOCKDOWN_WORK_INIT_PERCENTAGE = 50
-const LOCKDOWN_START_WORK_RECUPERATION_DATE = ''
-const LOCKDOWN_WORK_RECUPERATION_RATE_PER_MONTH = 10
+const LOCKDOWN_WORK_INIT_PERCENTAGE = 50 // x% of all individuals who will be selected to work at home
+const LOCKDOWN_WORK_RECUPERATION_RATE = 20 // x% of 100% working from home individuals
+const workRecuperationsDates = [
+  '01-03-2021',
+  '01-04-2021',
+  '01-05-2021',
+  '01-06-2021',
+  '01-07-2021'
+]
 
-export function assignLockdown(date: string, population: Individual[]) {
-  if (LOCKDOWN_INIT_DATE === date) startLockdown(population)
-  if (LOCKDOWN_START_SCHOOLS_RECUPERATION_DATE === date) startSchoolRecuperation(population)
-  if (LOCKDOWN_START_WORK_RECUPERATION_DATE === date) startWorkRecuperation(population)
+export function assignLockdown(day: string, month: number, year: number, population: Individual[]) {
+  const date = `${day}-${month}-${year}`
+
+  if (LOCKDOWN_INIT_DATE === date) start(population)
+
+  if (schoolRecuperationsDates.includes(date))
+    schoolRecuperation(
+      population,
+      schoolRecuperationsDates.indexOf(date) * LOCKDOWN_SCHOOL_RECUPERATION_RATE
+    )
+  if (workRecuperationsDates.includes(date)) workRecuperation(population)
 }
 
-function startLockdown(population: Individual[]) {
+function start(population: Individual[]) {
   for (const individual of population) {
-    if (individual.occupationTypes.includes('study')) {
-      for (let i = 0; i < individual.routine.length; i++) {
-        for (let j = 0; j < individual.routine[i].length; j++) {
-          if (individual.routine[i][j].category === 'study') {
-            switch (individual.routine[i][j].label) {
-              case 'study.preschool':
-                individual.routine[i][j] = preschoolStudyFromHome
-                break
-              case 'study.middle_school':
-                individual.routine[i][j] = middleSchoolStudyFromHome
-                break
-              case 'study.high_school':
-                individual.routine[i][j] = highSchoolStudyFromHome
-                break
-              case 'study.college':
-                individual.routine[i][j] = collegeStudyFromHome
-                break
-            }
-          }
+    if (individual.occupationTypes.includes('study')) implementSchoolFromHome(individual)
+
+    if (individual.occupationTypes.includes('work')) implementSchoolFromHome(individual)
+  }
+}
+
+function implementWorkFromHome(individual: Individual) {
+  for (let i = 0; i < individual.routine.length; i++) {
+    for (let j = 0; j < individual.routine[i].length; j++) {
+      if (individual.routine[i][j].category === 'study') {
+        switch (individual.routine[i][j].label) {
+          case 'study.preschool':
+            individual.routine[i][j] = preschoolStudyFromHome
+            break
+          case 'study.middle_school':
+            individual.routine[i][j] = middleSchoolStudyFromHome
+            break
+          case 'study.high_school':
+            individual.routine[i][j] = highSchoolStudyFromHome
+            break
+          case 'study.college':
+            individual.routine[i][j] = collegeStudyFromHome
+            break
         }
       }
-    }
-
-    if (individual.occupationTypes.includes('work')) {
     }
   }
 }
 
-function startSchoolRecuperation(population: Individual[]) {}
+function implementSchoolFromHome(individual: Individual) {}
 
-function startWorkRecuperation(population: Individual[]) {}
+function schoolRecuperation(population: Individual[], percentageRecuperate: number) {}
+
+function workRecuperation(population: Individual[]) {}
