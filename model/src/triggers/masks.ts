50f442eb2dd6cf6e70b8f2e58e5bea0373f1a36e
Gabriel W. Bartmanovicz <obielwb@gmail.com>
2024-02-28 07:42:07 -0300

    chore: add minor changes in masks trigger

--- model/src/triggers/masks.ts
+++ model/src/triggers/masks.ts
@@ -1,3 +1,14 @@
+export type MaskRegister = {
+  date: Date,
+}
+
 export class Mask() {
+
+  private maskDates: MaskRegister[]
+
+  constructor(
+    public population: Individual[]
+  ) {}
+
   public assign(currentDate: Date) {}
 }
