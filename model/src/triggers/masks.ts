0e53f97bc1ed106f4707cc65aa2f9db33267b1cc
Gabriel W. Bartmanovicz <obielwb@gmail.com>
2024-02-28 21:57:14 -0300

    feat(triggers): finish masks trigger

--- model/src/triggers/masks.ts
+++ model/src/triggers/masks.ts
@@ -1,14 +1,68 @@
+import { MaskType } from '../data'
+import { Individual, Occupation, OccupationType } from '../individual'
+import { shuffle } from '../utilities'
+
 export type MaskRegister = {
-  date: Date,
+  date: Date
+  type: MaskType
+  populationPercentage: number
+  options?: Partial<{
+    age: number[]
+    sex: 'male' | 'female'
+    income: number[]
+    transportationMean: 'private' | 'public'
+    occupationTypes: [OccupationType?, OccupationType?]
+    occupations: [Occupation?, Occupation?]
+  }>
 }
 
-export class Mask() {
-
+// todo: assign another name for MaskRegister and maskDates
+export class MaskTrigger {
   private maskDates: MaskRegister[]
 
-  constructor(
-    public population: Individual[]
-  ) {}
+  constructor(public population: Individual[], maskDates: MaskRegister[]) {
+    this.maskDates = maskDates
+  }
 
-  public assign(currentDate: Date) {}
+  public assign(currentDate: Date) {
+    const matchMaskDate = this.maskDates.find((maskDate) => maskDate.date === currentDate)
+
+    if (matchMaskDate !== undefined) {
+      this.implementMaskDistribution(matchMaskDate)
+    }
+  }
+
+  private implementMaskDistribution(maskImplementation: MaskRegister) {
+    this.cleanCurrentMasks()
+
+    const shuffledPopulation = shuffle(this.population)
+    // verify if individual is according option. If option is defined and not found in current individual, they dont go into matchedIndividuals
+    let matchedIndividuals = shuffledPopulation.filter(
+      (individual) =>
+        (maskImplementation.options.age === undefined ||
+          individual.age === maskImplementation.options.age) &&
+        (maskImplementation.options.sex === undefined ||
+          individual.sex === maskImplementation.options.sex) &&
+        (maskImplementation.options.income === undefined ||
+          individual.income === maskImplementation.options.income) &&
+        (maskImplementation.options.transportationMean === undefined ||
+          individual.transportationMean === maskImplementation.options.transportationMean) &&
+        (maskImplementation.options.occupationTypes === undefined ||
+          individual.occupationTypes === maskImplementation.options.occupationTypes) &&
+        (maskImplementation.options.occupations === undefined ||
+          individual.occupations === maskImplementation.options.occupations)
+    )
+
+    const numAffectedPeople = maskImplementation.populationPercentage * matchedIndividuals.length
+
+    matchedIndividuals.slice(0, numAffectedPeople).forEach((individual) => {
+      individual.mask = maskImplementation.type
+    })
+  }
+
+  private cleanCurrentMasks() {
+    this.population.forEach((individual) => {
+      individual.mask = 'none'
+    })
+  }
 }
