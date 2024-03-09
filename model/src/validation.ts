eec5a29f618036cad5979d181411315477fa9e7f
Gabriel W. Bartmanovicz <gabriel.bartmanovicz@ifood.com.br>
2024-03-09 09:37:15 -0300

    feat: create calculateModelAccuracy function

---
+++ model/src/validation.ts
@@ -0,0 +1,6 @@
+import { PandemicRegister } from '../data/covid19'
+
+export function calculateModelAccuracy(
+  pandemicRegisters: PandemicRegister[],
+  simulatedPandmicRegisters: PandemicRegister[]
+) {}
