c74cdacdb6df906bd80424fa09bdd1a1e9989ee7
Gabriel W. Bartmanovicz <obielwb@gmail.com>
2024-03-16 19:53:30 -0300

    feat: create results page

--- web/components/layout/header.tsx
+++ web/components/layout/header.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link'
 import { useEffect, useState } from 'react'
 import { ChevronRight } from 'lucide-react'
 import { usePathname } from 'next/navigation'
+import { Menu } from 'lucide-react'
 
 import PandemicaHeading from '@/components/pandemica-heading'
 import { Button } from '@/components/ui/button'
@@ -14,6 +15,10 @@ const links = [
   {
     url: '/paper',
     label: 'Artigo'
+  },
+  {
+    url: '/results',
+    label: 'Resultados'
   },
   {
     url: '/references',
@@ -31,6 +36,8 @@ const links = [
 
 export default function Header() {
   const [isScrolled, setIsScrolled] = useState(false)
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false)
+
   const pathname = usePathname()
 
   const size = useWindowSize()
@@ -46,58 +53,93 @@ export default function Header() {
   }, [])
 
   return (
-    <header
+    <div
       className={cn(
         'sticky top-0 z-50 h-14 border-b border-transparent bg-transparent backdrop-blur-xl transition-all duration-200 ease-linear',
         isScrolled && 'dark:border-muted border-neutral-200'
       )}
     >
-      <nav className="mx-auto flex h-[52px] w-full max-w-5xl flex-row justify-start gap-x-4 px-6 py-2 md:max-w-7xl md:gap-x-10">
-        <Link className="flex items-center justify-center" href="/">
-          <PandemicaHeading />
-        </Link>
-        <div className="flex w-full justify-end md:justify-between">
-          <div className="flex items-center gap-3 sm:gap-6">
-            {links.map((link, i) => {
-              if (link.url === '/references' && size.width <= 768) {
-                link.label = 'Ref.'
-              }
+      <header>
+        <nav className="mx-auto flex h-[52px] w-full max-w-5xl flex-row justify-between gap-x-4 px-6 py-2 md:max-w-7xl md:gap-x-10 ">
+          <Link className="flex items-center justify-center" href="/">
+            <PandemicaHeading />
+          </Link>
+          <div className="hidden w-full justify-end md:justify-between sm:flex">
+            <div className="flex items-center gap-3 sm:gap- 6">
+              {links.map((link, i) => {
+                if (link.url === '/references' && size.width <= 768) {
+                  link.label = 'Ref.'
+                }
 
-              return (
-                <Link
-                  key={`header-ref-${i}`}
-                  className="text-muted-foreground hover:text-foreground flex h-0 items-center text-sm font-normal transition-colors duration-200"
-                  href={
-                    link.url === '/paper' ? (pathname === '/' ? link.url : '/simulate') : link.url
-                  }
-                >
-                  {link.label === 'Artigo'
-                    ? pathname === '/'
-                      ? link.label
-                      : 'Simular'
-                    : link.label}
-                </Link>
-              )
-            })}
+                return (
+                  <Link
+                    key={`header-ref-${i}`}
+                    className="text-muted-foreground hover:text-foreground flex h-0 items-center text-sm font-normal transition-colors duration-200"
+                    href={
+                      link.url === '/paper' ? (pathname === '/' ? link.url : '/simulate') : link.url
+                    }
+                  >
+                    {link.label === 'Artigo'
+                      ? pathname === '/'
+                        ? link.label
+                        : 'Simular'
+                      : link.label}
+                  </Link>
+                )
+              })}
+            </div>
+            <div className="hidden flex-row items-center gap-x-4 md:flex">
+              <Link
+                href={
+                  pathname === '/' || pathname === '/paper' || pathname === '/references'
+                    ? '/simulate'
+                    : '/paper'
+                }
+              >
+                <Button className="flex h-9 items-center gap-x-0.5 rounded-full font-medium">
+                  {pathname === '/' || pathname === '/paper' || pathname === '/references'
+                    ? 'Simular'
+                    : 'Artigo'}
+                  <ChevronRight className="-mr-1" height={16} width={16} />
+                </Button>
+              </Link>
+            </div>
           </div>
-          <div className="hidden flex-row items-center gap-x-4 md:flex">
-            <Link
-              href={
-                pathname === '/' || pathname === '/paper' || pathname === '/references'
-                  ? '/simulate'
-                  : '/paper'
-              }
+
+          <div className="flex sm:hidden">
+            <Button
+              className="bg-transparent dark:bg-transparent dark:hover:bg-transparent"
+              onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
             >
-              <Button className="flex h-9 items-center gap-x-0.5 rounded-full font-medium">
-                {pathname === '/' || pathname === '/paper' || pathname === '/references'
-                  ? 'Simular'
-                  : 'Artigo'}
-                <ChevronRight className="-mr-1" height={16} width={16} />
-              </Button>
-            </Link>
+              <span className="sr-only">Abrir Menu</span>
+              <Menu className="text-black dark:text-white" />
+            </Button>
           </div>
+        </nav>
+      </header>
+      {isMobileMenuOpen ? (
+        <div className="flex flex-col gap-6  bg-background z-50 px-9 transition-all duration-200 ease-linear border-2 border-muted  m-4 rounded ">
+          {links.map((link, i) => {
+            if (link.url === '/references' && size.width <= 768) {
+              link.label = 'Ref.'
+            }
+
+            return (
+              <Link
+                key={`header-ref-${i}`}
+                className="text-muted-foreground hover:text-foreground flex h-0 items-center text-sm font-normal transition-colors duration-200"
+                href={
+                  link.url === '/paper' ? (pathname === '/' ? link.url : '/simulate') : link.url
+                }
+              >
+                {link.label === 'Artigo' ? (pathname === '/' ? link.label : 'Simular') : link.label}
+              </Link>
+            )
+          })}
         </div>
-      </nav>
-    </header>
+      ) : (
+        <></>
+      )}
+    </div>
   )
 }
