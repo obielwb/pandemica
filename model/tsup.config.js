996f012579829a4aca7ad9aeb6c96440a865c70a
ojpbarbosa <joaopedrofbarbosa@protonmail.com>
2024-03-16 12:20:03 -0300

    chore(model): set up js transpilation

--- model/tsup.config.js
+++ model/tsup.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig({
   tsconfig: './tsconfig.json',
-  entry: ['./src'],
+  entry: ['./src', './data/census/index.ts', './data/covid19/index.ts'],
   splitting: false,
   sourcemap: true,
   clean: true,
-  minify: true,
+  minify: false,
   target: 'es2022'
 })
