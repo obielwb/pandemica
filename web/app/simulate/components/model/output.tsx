4ab9975a609b348d17e6f1fa7a92f04616e731d5
ojpbarbosa <joaopedrofbarbosa@protonmail.com>
2024-03-17 17:45:36 -0300

    feat(web): create data visualization

--- web/app/simulate/components/model/output.tsx
+++ web/app/simulate/components/model/output.tsx
@@ -1,6 +1,7 @@
 import { useContext, useState, useCallback, useEffect } from 'react'
 import { Textarea } from '@/components/ui/textarea'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
+import DataVisualization from './data-visualization'
 
 type ModelOutputProps = {
   configuration: {
@@ -12,18 +13,11 @@ type ModelOutputProps = {
 export default function Output({ configuration }: ModelOutputProps) {
   const [output, setOutput] = useState(['runId'])
 
-  const onModelOutput = useCallback(
-    (newOutput: string) => {
-      setOutput([...output, newOutput])
-    },
-    [output]
-  )
-
   return (
-    <Tabs defaultValue="logs" className="w-full">
+    <Tabs defaultValue="data" className="w-full">
       <TabsList className="w-full">
-        <TabsTrigger className="w-auto" value="charts">
-          Gráficos
+        <TabsTrigger className="w-auto" value="data">
+          Dados
         </TabsTrigger>
         <TabsTrigger className="w-auto" value="city">
           Cidade
@@ -35,7 +29,65 @@ export default function Output({ configuration }: ModelOutputProps) {
           Logs
         </TabsTrigger> */}
       </TabsList>
-      <TabsContent value="charts">Muitos, muitos gráficos.</TabsContent>
+      <TabsContent value="data" className="flex flex-col gap-y-4">
+        {/* todo: properly calculate accuracy */}
+        {/* todo: use papaparse to parse the last line of both csvs in order to plot the real value here */}
+        {/* <div className="flex flex-col gap-4 md:flex-row w-full">
+                  <section className="dark:border-muted gap-y-2 bg-background dark:bg-muted/50 rounded py-6 px-9 border-2 w-ful flex flex-col md:justify-center md:items-start relative">
+                    <p className="text-sm text-pandemica-yellow">Acurácia</p>
+                    <span className="text-7xl font-sans-heading">30%</span>
+                  </section>
+                  <section className="dark:border-muted gap-y-2 bg-background dark:bg-muted/50 rounded py-6 px-9 border-2 w-ful flex flex-col md:justify-center md:items-start relative">
+                    <p className="text-sm tracking-tight text-pandemica-yellow">Casos</p>
+                    <div className="grid grid-cols-2 grid-flow-rows md:grid-cols2 md:grid-flow-col gap-4">
+                      <div>
+                        <span className="text-sm font-semibold tracking-tight">Real</span>
+                        <p className="text-muted-foreground text-sm tracking-tight">209.500</p>
+                      </div>
+                      <div>
+                        <span className="text-sm font-semibold tracking-tight">Simulado</span>
+                        <p className="text-muted-foreground text-sm tracking-tight">724.645</p>
+                      </div>
+                    </div>
+                  </section>
+                  <section className="dark:border-muted gap-y-2 bg-background dark:bg-muted/50 rounded py-6 px-9 border-2 w-ful flex flex-col md:justify-center md:items-start relative">
+                    <p className="text-sm tracking-tight text-pandemica-yellow">Mortes</p>
+                    <div className="grid grid-cols-2 grid-flow-rows md:grid-cols-2 md:grid-flow-col gap-4">
+                      <div>
+                        <span className="text-sm font-semibold tracking-tight">Real</span>
+                        <p className="text-muted-foreground text-sm tracking-tight">5.360</p>
+                      </div>
+                      <div>
+                        <span className="text-sm font-semibold tracking-tight">Simulado</span>
+                        <p className="text-muted-foreground text-sm tracking-tight">19.313</p>
+                      </div>
+                    </div>
+                  </section>
+                </div> */}
+        <div className="flex flex-col w-full">
+          <DataVisualization />
+        </div>
+        <div className="w-full flex items-center justify-center px-8">
+          <section className="dark:border-muted bg-background dark:bg-muted/50 rounded p-5 border-2 flex flex-col w-full md:w-fit">
+            <p className="text-sm tracking-tight text-pandemica-yellow">Dados técnicos</p>
+            <div className="grid grid-cols-2 grid-flow-rows md:grid-cols-3 md:grid-flow-col gap-4">
+              <div>
+                <span className="text-sm font-semibold tracking-tight">Linhas de código</span>
+                <p className="text-muted-foreground text-sm tracking-tight">+8 mil</p>
+              </div>
+              <div>
+                <span className="text-sm font-semibold tracking-tight">Tempo de simulação</span>
+                <p className="text-muted-foreground text-sm tracking-tight">2 horas</p>
+              </div>
+              <div>
+                {/* todo: fetch from csv */}
+                <span className="text-sm font-semibold tracking-tight">Dias simulados</span>
+                <p className="text-muted-foreground text-sm tracking-tight">1.020</p>
+              </div>
+            </div>
+          </section>
+        </div>
+      </TabsContent>
       {/* todo?: perhaps remove the tabs bellow and focus only on the charts */}
       <TabsContent value="city">Dados da cidade em questão.</TabsContent>
       <TabsContent value="patology">Dados da patologia em questão.</TabsContent>
