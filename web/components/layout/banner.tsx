c74c868127536270751da5fcd60a4d339931e35d
ojpbarbosa <joaopedrofbarbosa@protonmail.com>
2024-10-19 12:06:42 -0300

    feat: add neural network description

--- web/components/layout/banner.tsx
+++ web/components/layout/banner.tsx
@@ -1,8 +1,23 @@
+'use client'
+
+import { cn } from '@/lib/utils'
 import Link from 'next/link'
+import { useEffect, useState } from 'react'
 
 export default function Banner() {
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
   return (
-    <div className="top-0 z-50 h-12 sm:h-8 border-b bg-pandemica-blue/70 dark:bg-pandemica-blue/50 border-pandemica-blue text-white flex items-center justify-center">
+    <div
+      className={cn(
+        'top-0 z-50 h-12 sm:h-8 border-b bg-pandemica-blue/70 dark:bg-pandemica-blue/50 border-pandemica-blue text-white flex items-center justify-center',
+        mounted && window && window.location.pathname === '/paper' ? 'hidden' : ''
+      )}
+    >
       <p className="sm:text-sm text-xs sm:p-0 p-5 text-center">
         <b>New ✨</b> Paper is now fully available in English.{' '}
         <Link href="/paper" className="underline underline-offset-2">
